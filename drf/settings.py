ca706fbb12a598a804b5956ef6e04c2e2d2e7d5b
Akash Garg <akash.garg@iaugmentor.com>
2020-01-13 10:51:36 +0530

    link MySQL database

--- drf/settings.py
+++ drf/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'drf.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+            'HOST': 'localhost',
+            'PORT': 3306,
+            'NAME': 'iaugmentor',
+            'USER': 'akashg14',
+            'PASSWORD': '1164',
         
     }
 }
